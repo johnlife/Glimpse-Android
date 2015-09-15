523dadf8203a32b054d55ab9631b0d8a6404a80e
kuzmenok <stanislav.kuzmenok@gmail.com>
2015-09-15 15:42:28 +0300

    add grid to gallery

--- src/odesk/johnlife/glimpse/adapter/ImagesGalleryAdapter.java
+++ src/odesk/johnlife/glimpse/adapter/ImagesGalleryAdapter.java
@@ -6,16 +6,50 @@ import odesk.johnlife.glimpse.data.FileHandler;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 public class ImagesGalleryAdapter extends BaseAdapter implements Constants {
 
 	private Context context;
 	private FileHandler fileHandler;
+	private final static int CACHE_CAPACITY = 50;
+	private final int SMALL_BITMAP_SIZE = 500;
+
+	private static final HashMap<Integer, Bitmap> hardBitmapCache = new LinkedHashMap<Integer, Bitmap>(CACHE_CAPACITY, 0.75f, true) {
+		@Override
+		protected boolean removeEldestEntry(LinkedHashMap.Entry<Integer, Bitmap> eldest) {
+			return size() > CACHE_CAPACITY;
+		}
+	};
+
+	private void putToCache(Integer key, Bitmap bitmap) {
+		if (bitmap != null) {
+			synchronized (hardBitmapCache) {
+				hardBitmapCache.put(key, bitmap);
+			}
+		}
+	}
+
+	private Bitmap getFromCache(Integer key) {
+		Bitmap bitmap;
+		synchronized (hardBitmapCache) {
+			bitmap = hardBitmapCache.get(key);
+			if (bitmap != null) {
+				hardBitmapCache.remove(key);
+				hardBitmapCache.put(key, bitmap);
+				return bitmap;
+			}
+		}
+		return bitmap;
+	}
 
 	public ImagesGalleryAdapter(Context context) {
 		this.context = context;
@@ -40,7 +74,11 @@ public class ImagesGalleryAdapter extends BaseAdapter implements Constants {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ImageView view = new ImageView(context);
-		Bitmap bitmap = BitmapFactory.decodeFile(fileHandler.getFiles().get(position).getPath());
+		Bitmap bitmap = getFromCache(position);
+		if (null == bitmap) {
+			bitmap = resizeToSmall(BitmapFactory.decodeFile(fileHandler.getFiles().get(position).getPath()));
+			putToCache(position, bitmap);
+		}
 		view.setImageBitmap(bitmap);
 		int sizeHeight = GlimpseApp.getScreen().getHeight();
 		int sizeWidth = GlimpseApp.getScreen().getWidth();
@@ -49,6 +87,18 @@ public class ImagesGalleryAdapter extends BaseAdapter implements Constants {
 		view.setLayoutParams(new GridView.LayoutParams(sizeWidth, sizeHeight));
 		view.setScaleType(ImageView.ScaleType.FIT_XY);
 		return view;
+	}
+
+	private Bitmap resizeToSmall(Bitmap original) {
+		int originalSize = Math.max(original.getWidth(), original.getHeight());
+		float scale = 1;
+		while (originalSize > SMALL_BITMAP_SIZE) {
+			originalSize /= 2;
+			scale /= 2;
+		}
+		Matrix m = new Matrix();
+		m.postScale(scale, scale);
+		return Bitmap.createBitmap(original, 0, 0, original.getWidth(), original.getHeight(), m, false);
 	}
 	
 }
